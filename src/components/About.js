86580607abacecc09712a9d69bd112122fe93e4e
supankarchb <supankar710@gmail.com>
2021-10-05 23:28:58 +0600

    finalCommit

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,11 @@ const About = () => {
     return (
         <div>
              <h1 className="p-3">Why we <strong className="text-danger">are Best</strong></h1>
-            <p className="p-3 m-2"><b>ABC Coaching Center Transformation Academy provides world-class online coach certification training programs to aspiring coaches and professionals looking for pathways to the International Coaching Federation Credentialing and Certification. Our online coaching certification courses are fully interactive, ICF-Accredited, and flexible – providing the freedom to learn to coach anywhere on-the-go.
+            <p className="p-3 m-2 bg-dark text-white rounded">ABC Coaching Center Transformation Academy provides world-class online coach certification training programs to aspiring coaches and professionals looking for pathways to the International Coaching Federation Credentialing and Certification. Our online coaching certification courses are fully interactive, ICF-Accredited, and flexible – providing the freedom to learn to coach anywhere on-the-go.
 
             From the NLP Practitioner and Coach to Train The Trainer, our online ICF training programs are fully interactive and self-paced, so you can work on them whenever you want. Our Certified Team Transformation Coach Programs equips the leaders and professionals with the right tools and skills to drive the change on organizational-level through coaching. We also help you discover your coaching niche with our specialized professional certified coaching online programs.
 
-            Our online accredited coach training programs are segregated into carefully designed coaching training hours, mentoring, and assessments to give you real-time coaching training with our global coaching community.</b></p>
+            Our online accredited coach training programs are segregated into carefully designed coaching training hours, mentoring, and assessments to give you real-time coaching training with our global coaching community.</p>
         </div>
     );
 };
--- src/components/Homes.js
+++ src/components/Homes.js
@@ -9,6 +9,8 @@ const Homes = () => {
         <div>
             <Header/>
             <Home></Home>
+            <h1 className="p-3"><b>Our Achievement</b></h1>
+            <p className="p-3 m-2 bg-secondary text-white rounded-pill">ABC Coaching Center are renowned in Dhaka City.Many student got chances from our coaching.WE got many prices in coaching section.Recorded students may got chance from us.Our have a reputed caching in the cityFrom the NLP Practitioner and Coach to Train The Trainer, our online ICF training programs are fully interactive and self-paced, so you can work on them whenever you want. Our Certified Team Transformation Coach Programs equips the leaders and professionals with the right tools and skills to drive the change on organizational-level through coaching.</p>
             <Data></Data>
             <Footer></Footer>
         </div>
